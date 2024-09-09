25a7fac89aa359223f565d037913a359ff1c7e93
Alex Ryan <alexanderfryan@gmail.com>
2024-09-09 02:21:38 -0400

    Implement drop. Styles

--- src/App.tsx
+++ src/App.tsx
@@ -8,45 +8,75 @@ import {
 } from "@radix-ui/themes";
 import { useEffect, useState } from "react";
 
-function DataTransferItemValue({ item }: { item: DataTransferItem }) {
-  const [value, setValue] = useState<string | File | null>(() => "");
-  useEffect(() => {
-    setValue("");
-    let canceled = false;
-    if (item.kind === "string") {
-      item.getAsString((stringValue) => {
-        if (!canceled) {
-          setValue(stringValue);
-        }
-      });
-    } else {
-      setValue(item.getAsFile());
+type SerializedDataTransferItem =
+  | {
+      kind: "string";
+      type: string;
+      stringValue: string;
     }
-
-    return () => {
-      canceled = true;
+  | {
+      kind: "file";
+      type: string;
+      fileValue: File | null;
     };
-  }, [item]);
 
-  return value instanceof File ? `<File ${value.name}>` : value;
-}
+/**
+ * DataTransfer handles are only valid through the duration of the drag/paste operation,
+ * so they can't be persisted in React state and rendered directly.
+ *
+ * They also don't implement structuredClone(), which is annoying
+ */
+type SerializedDataTransfer = {
+  items: SerializedDataTransferItem[];
+};
 
-function itemsToArray(dataTransfer: DataTransfer) {
-  return new Array(dataTransfer.items.length)
+async function serializeDataTransfer(
+  dt: DataTransfer
+): Promise<SerializedDataTransfer> {
+  // DataTransfer doesn't implement Iterable, so we array-ify it manually
+  const itemsArray = new Array(dt.items.length)
     .fill(0)
-    .map((_, idx) => dataTransfer.items[idx]);
+    .map((_, i) => dt.items[i]);
+
+  const serializedItems = await Promise.all(
+    itemsArray.map(async (item): Promise<SerializedDataTransferItem> => {
+      if (item.kind === "string") {
+        return {
+          kind: "string",
+          type: item.type,
+          stringValue: await new Promise<string>((resolve) =>
+            item.getAsString(resolve)
+          ),
+        };
+      } else if (item.kind === "file") {
+        return {
+          kind: "file",
+          type: item.type,
+          fileValue: item.getAsFile(),
+        };
+      } else {
+        throw new Error(`Unexpected item.kind: ${item.kind}`);
+      }
+    })
+  );
+
+  return {
+    items: serializedItems,
+  };
 }
 
-function DataTransferView({ dt }: { dt: DataTransfer | null }) {
+function DataTransferView({ dt }: { dt: SerializedDataTransfer | null }) {
   return (
     <DataList.Root>
       {dt != null &&
-        itemsToArray(dt).map((item, index) => (
+        dt.items.map((item, index) => (
           <DataList.Item key={index}>
             <DataList.Label className="mime-label">{item.type}</DataList.Label>
             <DataList.Value>
               <Code>
-                <DataTransferItemValue item={item} />
+                {item.kind === "file"
+                  ? `<File ${item.fileValue?.name ?? ""}>`
+                  : item.stringValue}
               </Code>
             </DataList.Value>
           </DataList.Item>
@@ -55,10 +85,43 @@ function DataTransferView({ dt }: { dt: DataTransfer | null }) {
   );
 }
 
+function useWindowEvent<K extends keyof WindowEventMap>(
+  event: K,
+  handler: (event: WindowEventMap[K]) => void,
+  deps: unknown[]
+) {
+  useEffect(() => {
+    window.addEventListener(event, handler);
+    return () => {
+      window.removeEventListener(event, handler);
+    };
+  }, deps);
+}
+
 function InputArea() {
-  const [dataTransfer, setDataTransfer] = useState<DataTransfer | null>(
-    () => null
+  const [isWindowDragging, setIsWindowDragging] = useState(() => false);
+  const [isDragEnter, setIsDragEnter] = useState(() => false);
+
+  const [dataTransfer, setDataTransfer] =
+    useState<SerializedDataTransfer | null>(() => null);
+
+  useWindowEvent(
+    "dragstart",
+    () => {
+      setIsWindowDragging(true);
+    },
+    []
   );
+
+  useWindowEvent(
+    "dragend",
+    () => {
+      setIsWindowDragging(false);
+    },
+    []
+  );
+
+  console.log("render", dataTransfer);
 
   return (
     <>
@@ -66,11 +129,36 @@ function InputArea() {
         Input
       </Heading>
       <TextField.Root
+        className={`inputarea ${isWindowDragging ? "-window-dragging " : ""} ${
+          isDragEnter ? "-drag-enter" : ""
+        }`}
         size="3"
-        onPaste={(evt) => {
-          setDataTransfer(evt.clipboardData);
+        onPaste={async (evt) => {
+          try {
+            setDataTransfer(await serializeDataTransfer(evt.clipboardData));
+          } catch (e) {
+            console.error(e);
+          }
         }}
-        placeholder="Paste or drag here to inspect clipboard data"
+        onDragOver={(evt) => {
+          // required to mark this as a compatible drop zone
+          evt.preventDefault();
+        }}
+        onDragEnter={() => {
+          setIsDragEnter(true);
+        }}
+        onDragLeave={() => {
+          setIsDragEnter(false);
+        }}
+        onDrop={async (evt) => {
+          setIsDragEnter(false);
+          try {
+            setDataTransfer(await serializeDataTransfer(evt.dataTransfer));
+          } catch (e) {
+            console.error(e);
+          }
+        }}
+        placeholder="Paste or drop here to inspect clipboard data"
         value=""
         onChange={() => {
           // we don't want to listen to the value; make the React warning go away
@@ -89,6 +177,17 @@ export function App() {
         <Heading size="6" mb="4">
           DataTransfer Inspector
         </Heading>
+        <p>
+          A simple tool to inspect the HTML5 DataTransfer{" "}
+          <a href="https://developer.mozilla.org/en-US/docs/Web/API/DataTransfer">
+            [mdn]
+          </a>{" "}
+          MIME types associated with paste and drag-and-drop events.
+        </p>
+        <p>
+          To use this tool, simply paste or drop a payload into the box below.
+          All clientside, no network requests ever.
+        </p>
         <InputArea />
       </Box>
     </Box>
